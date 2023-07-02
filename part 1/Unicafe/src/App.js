6503b78ea9539ecb9fbf14246bf734554d6fb70c
Ozcan Sert <ozcsert@gmail.com>
2023-07-02 23:56:31 +0300

    unicafe 1.9 extracted statistics component

--- part 1/Unicafe/src/App.js
+++ part 1/Unicafe/src/App.js
@@ -4,32 +4,54 @@
 
 import { useState } from "react";
 
+const Statistics = (props) => {
+  if(props.allCounter === 0) {
+    return (
+      <>
+      <h1>Statistics</h1>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+  return (
+    <>
+    <h1>Statistics</h1>
+    <p> {props.text} {props.goodVal}</p>
+    <p> {props.text2} {props.neutralVal}</p>
+    <p> {props.text3} {props.badVal}</p>
+    <p> {props.text4} {props.allCounter}</p>
+    <p> {props.text5} {props.average} </p>
+    <p> {props.text6} {props.positive}</p>
+    </>
+  )
+}
+
 function App() {
 
-  const [goodValue, setGoodValue] = useState(0)
+  const [goodVal, setGoodVal] = useState(0)
   const [neutralVal, setNeutralVal] = useState(0)
   const [badVal, setBadVal] = useState(0)
 
 
-  const allCounter = (a,b,c) =>  a + b + c
+  const allCounter = () =>  goodVal + badVal + neutralVal
 
-  const average = () => (goodValue - badVal) / (goodValue + neutralVal + badVal)
+  const average = () => (goodVal - badVal) / (goodVal + neutralVal + badVal)
 
-  const positive = () => (goodValue) / (goodValue + neutralVal + badVal) *100 
+  const positive = () => (goodVal) / (goodVal + neutralVal + badVal) *100 
   return (
     <>
       <h1>Give Feedback</h1>
-      <button onClick={() => setGoodValue(goodValue +1)}>good</button>
+      <button onClick={() => setGoodVal(goodVal +1)}>good</button>
       <button onClick={() => setNeutralVal(neutralVal +1)}>neutral</button>
       <button onClick={() => setBadVal(badVal+1)}>bad</button>
-      <h1>Statistics</h1>
-      <p >good {goodValue} </p>
-      <p >neutral {neutralVal} </p>
-      <p >bad {badVal} </p>
-      <p>all {allCounter(goodValue,badVal,neutralVal)}</p>
-      <p>average {average()} </p>
-      <p>positive {positive()} %</p>
-      
+      <Statistics 
+      text="good" goodVal={goodVal}
+      text2="neutral" neutralVal={neutralVal}
+      text3="bad" badVal={badVal}
+      text4="all" allCounter={allCounter()}
+      text5="average" average={average()}
+      text6="positive" positive={positive()}
+      />
     </>
   );
 }
