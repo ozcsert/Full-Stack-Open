2313db69dc916f99e1d0906530fa56fbc7eed51c
Ozcan Sert <ozcsert@gmail.com>
2023-07-04 16:31:53 +0300

    new branch commit

--- part 2/2.1 Course/src/App.js
+++ part 2/2.1 Course/src/App.js
@@ -1,72 +1,104 @@
-
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
     <>
-    <h1> {props.course}</h1>
+      <h1> {course}</h1>
     </>
-  )
-}
+  );
+};
 
-const Part = (props) => {
+const Part = ({ part, exercises }) => {
   return (
-    <p> {props.part} {props.exercises} </p>
-  )
-}
+    <p>
+      {" "}
+      {part} {exercises}{" "}
+    </p>
+  );
+};
 
-const Content = (props) => {
+const Content = ({ parts }) => {
+ 
   return (
     <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+      <Part part={parts[3].name} exercises={parts[3].exercises} />
+      
     </>
-  )
-}
+  );
+};
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, order) => s + order.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
 
 const Course = () => {
-  const course = {
-    id: 1,
-    name : 'Half Stack application development',
-    parts : [
-      {
-        id: 1,
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        id: 3,
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        id: 4,
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+  const course = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  console.log(course[0].name)
   return (
     <>
-    <Header course = {course.name}/> 
-    <Content parts = {course.parts} />
+      <Header course={course[0].name} />
+      <Content parts={course[0].parts} />
+      <Total parts={course[0].parts} />
+      <Header course={course[1].name} />
+      <Content parts={course[0].parts} />
+      <Total parts={course[1].parts} />
+      
     </>
-  )
-}
+  );
+};
 
 const App = (course) => {
-  
   return (
     <>
-    <Course course={course}/>
+      <Course course={course} />
     </>
-  )
-}
+  );
+};
 
 export default App;
