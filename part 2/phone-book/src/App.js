b690ebb31df098784b0524bba88c99d1873476e4
Ozcan Sert <ozcsert@gmail.com>
2023-07-05 19:49:08 +0300

    PhoneBook 2.7

--- part 2/phone-book/src/App.js
+++ part 2/phone-book/src/App.js
@@ -1,22 +1,58 @@
 import { useState } from 'react'
+import './App.css';
 
 function App() {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'}
+    { name: 'Arto Hellas',
+      number : +90-552-8985666,
+      id: 1
+  }
   ])
-  const [newName, setNewName] = useState('')
+
+  const [newName, setNewName] = useState('enter a name')
+  
+  const addPerson = (event) => {
+    event.preventDefault()
+    console.log("person added", event.target)
+    const contacts = {
+      name : newName,
+      id : persons.length+1
+    }
+    if ( persons.find(person => person.name === newName)===undefined) {
+      setPersons(persons.concat(contacts))
+      setNewName("")
+    } alert(`${newName} is already added to phonebook`)
+      
+  }
+
+
+  const newValueChange = (event) => {
+
+    console.log(event.target.value)
+    setNewName(event.target.value)
+
+  }
+
   return (
     <>
     <h2>Phonebook</h2>
-    <form>
+    <form onSubmit={addPerson}>
       <>
-      name: <input />
+      name: <input onChange={newValueChange} />
       </>
       <>
-      <button type="submit">add</button>
+      <button type="submit" >add</button>
       </>
     </form>
     <h2>Numbers</h2>
+    <ul>
+    {persons.map(person =>
+      <li key={person.id}>
+        {person.name}
+      </li>
+      )}
+      
+    </ul>
     </>
   );
 }
