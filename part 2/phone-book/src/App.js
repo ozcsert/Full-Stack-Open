514e93feb872be44ccc35106842d6d0b6146250f
Ozcan Sert <ozcsert@gmail.com>
2023-07-07 15:58:51 +0300

    useEffect to retrieve data form the server

--- part 2/phone-book/src/App.js
+++ part 2/phone-book/src/App.js
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css';
+import axios from 'axios';
 
 const InputChangeLine = ({ text, onChange, value, onClick}) => {
   return (
@@ -45,12 +46,17 @@ const Contacts = ({newFilter, persons, filteredPersons,}) => {
 } 
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  
+  //Get the data from the server(db.json).
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        console.log(response.data)
+        setPersons(response.data)
+      })
+  },[])
 
   const [newName, setNewName] = useState('enter a name...')
   const [newNumber, setNewNumber] = useState("and the number...")
