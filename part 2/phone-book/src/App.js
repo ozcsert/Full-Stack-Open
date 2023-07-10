0f1022da6274695f005af822a83fb4634b2fcf32
Ozcan Sert <ozcsert@gmail.com>
2023-07-10 18:47:45 +0300

    The Phonebook 2.15 delete,update, back-end server added

--- part 2/phone-book/src/App.js
+++ part 2/phone-book/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css';
-import axios from 'axios';
+
+import personService from './services/persons'
 
 const InputChangeLine = ({ text, onChange, value, onClick}) => {
   return (
@@ -24,7 +25,7 @@ const Form = ({onSubmit, newNameChange, newNumberChange, name, number, newName, 
 
 const Header = ({text}) => <h2>{text}</h2>
 
-const Contacts = ({newFilter, persons, filteredPersons,}) => {
+const Contacts = ({newFilter, persons, filteredPersons, deleteContactOf}) => {
   return (
     <ul>
     {(newFilter === "filter by..." || newFilter == "") 
@@ -32,12 +33,14 @@ const Contacts = ({newFilter, persons, filteredPersons,}) => {
     persons.map(person =>
       <li key={person.id}>
         {person.name} {person.number}
+        <button type="button" onClick={() =>deleteContactOf(person.id)} >delete</button>
       </li>
       )
     :
     filteredPersons.map(person =>
       <li key={person.id}>
         {person.name} {person.number}
+        <button type="button" onClick={() =>deleteContactOf(person.id)}  >delete</button>
       </li>
       )}
     </ul>
@@ -49,14 +52,14 @@ function App() {
   const [persons, setPersons] = useState([])
   
   //Get the data from the server(db.json).
+
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/persons")
-      .then(response => {
-        console.log(response.data)
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
       })
-  },[])
+  }, [])
 
   const [newName, setNewName] = useState('enter a name...')
   const [newNumber, setNewNumber] = useState("and the number...")
@@ -68,15 +71,26 @@ function App() {
       id : persons.length+1,
       number : newNumber
     }
-    if ( persons.find(person => person.name.toUpperCase() === newName.toUpperCase())===undefined && newNumber != "") {
-      setPersons(persons.concat(contacts))
-      setNewName("")
-      setNewNumber("")
-    } else if  (newNumber === "") {
-    alert("Please enter the number")
-    } else
-    alert(`${newName} is already added to phonebook`)   
+
+    if (persons.find(person => person.name.toUpperCase() === newName.toUpperCase())===undefined && newNumber != "and the number..." && newNumber != "") {
+      //New posts are saved to the db
+      personService
+        .create(contacts)
+        .then(response => {
+          setPersons(persons.concat(response))
+        },[])
+        setNewName('enter a name...')
+        setNewNumber("and the number...")
+    } else if  (persons.find(person => person.name.toUpperCase() === newName.toUpperCase())!== ""  && newNumber === "and the number..." || newNumber === "") {
+    alert("Please enter the number") 
+  } else if (persons.find(person => person.name.toUpperCase() === newName.toUpperCase())) {
+    if (window.confirm(`${newName.name} is already added to the phonebook, replace the old number with a new one ?`)) {
+  //Finds the entered person and updates the new number with the old one.
+  updateContact()
+      }
   }
+}
+  
 
   const newNameChange = (event) => {
     console.log(event.target.value)
@@ -96,8 +110,36 @@ function App() {
   const deleteInput = (a) => {
     console.log(a.target.value)
     a.target.value = ""
-    
   }
+
+  const deleteContactOf = (id) => {
+    const url = `http://localhost:3001/persons/${id}`
+    const deletedPerson = persons.filter(n => n.id === id)
+    const unaffectedPersons = persons.filter(n => n.id !== id)
+    console.log(unaffectedPersons);
+    if (window.confirm(`Do you really want to send ${deletedPerson[0].name} into oblivion?`)) {
+    setPersons(unaffectedPersons)
+    personService
+      .remove(id)
+      alert(`${deletedPerson[0].name} has been deleted from the list.`)
+  }  }
+
+  const updateContact = (id) => {
+  
+    const indexOfPerson = persons.map(e => e.name.toUpperCase()).indexOf(`${newName.toUpperCase()}`);
+    const person = persons.filter((person) => person.name === newName)
+    const obj = {number:newNumber}
+    const changedPerson = {...persons[indexOfPerson], ...obj}//Object.assign(person, obj)
+    console.log(indexOfPerson);
+    personService
+      .update(changedPerson.id, changedPerson)
+      .then(returnedPerson => 
+      setPersons(persons.map(person => person.id !== persons[indexOfPerson].id ? person: returnedPerson))
+      )
+      setNewName('enter a name...')
+      setNewNumber("and the number...")
+  }
+
   const filteredPersons = persons.filter(person => person.name.toUpperCase().includes(newFilter.toUpperCase() || newFilter == 'filter by...'))
   
   return (
@@ -107,7 +149,7 @@ function App() {
     <Header text="Create a contact" />
     <Form onSubmit={addPerson} deleteInput={deleteInput}  name="name" newName={newName} newNameChange={newNameChange} number="number" newNumber={newNumber} newNumberChange={newNumberChange}  />
     <Header text="Contacts" />
-    <Contacts newFilter={newFilter} persons={persons} filteredPersons={filteredPersons} />
+    <Contacts newFilter={newFilter} persons={persons} filteredPersons={filteredPersons} deleteContactOf={deleteContactOf} />
     </>
   );
 }
