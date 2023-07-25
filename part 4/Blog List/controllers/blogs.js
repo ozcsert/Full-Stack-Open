792aa74447546b42cb7f69b7a32681a19e9e31ea
Ozcan Sert <ozcsert0@gmail.com>
2023-07-25 13:07:49 +0300

    Blog List 4.11

--- part 4/Blog List/controllers/blogs.js
+++ part 4/Blog List/controllers/blogs.js
@@ -11,11 +11,20 @@ blogsRouter.get("/", (request, response) => {
 
 blogsRouter.post("/", (request, response) => {
   const blog = new Blog(request.body)
+  console.log(blog.likes);
+  //if (blog.likes === u)
+  blog.likes 
+  ? blog.likes = blog.likes
+  : blog.likes = 0
+
+  console.log(blog.likes);
+
   blog
     .save()
     .then(result => {
       response.status(201).json(result)
     })
+    .catch(error => error.message)
 })
 
 module.exports = blogsRouter
--- part 4/Blog List/tests/blog_api.test.js
+++ part 4/Blog List/tests/blog_api.test.js
@@ -3,6 +3,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const config = require("../utils/config")
+const blog = require ("../models/blog")
 
 mongoose.connect(config.MONGODB_URI)
 .then(() => {
@@ -29,7 +30,7 @@ test('the unique identifier to be named: id', async () => {
 
 test('that validates new posts are created', async () => {    
     const firstResponse = await api.get('/api/blogs')
-
+    
     const newBlog= { 
         title: "added",
         author: "added",
@@ -43,8 +44,26 @@ test('that validates new posts are created', async () => {
         .expect('Content-Type', /application\/json/)
 
         const secondResponse = await api.get('/api/blogs')
-        expect(secondResponse.body).toHaveLength(firstResponse.body.length + 1)
+        await expect(secondResponse.body).toHaveLength(firstResponse.body.length + 1)
+        await blog.deleteOne()
+})
 
+test('likes property defaults to 0, given it is undefined', async () => {
+    const newBlog= { 
+        title: "added",
+        author: "added",
+        url:"added",
+    }
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+        const request = await api.get('/api/blogs')
+        const lastAddedObject = request.body[request.body.length - 1]
+        
+        expect(lastAddedObject.likes).toBe(0)
 })
 
 
