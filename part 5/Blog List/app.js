716b6fceae9364c00ffe241f7427b007f3c84da1
Ozcan Sert <ozcsert0@gmail.com>
2023-08-09 15:56:06 +0300

    unicafe revisited 6.2 Redux 1

--- part 5/Blog List/app.js
+++ part 5/Blog List/app.js
@@ -27,10 +27,12 @@ app.use(express.static("build"))
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", userRouter)
 app.use("/api/login", loginRouter)
+
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
+
 app.use(middleware.errorHandler)
 
 module.exports = app
